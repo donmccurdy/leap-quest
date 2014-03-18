851a80d803cde8aa8272a3103b8648cc53bb2a0c
Don McCurdy <don.r.mccurdy@gmail.com>
2014-03-18 08:59:42 -0700

    Correcting port used.

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
+var settings = require('./settings');
 
 // Launch web server
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || settings.QUEST_PORT);
 
 // Launch game server
 require('./server/start')(server);
